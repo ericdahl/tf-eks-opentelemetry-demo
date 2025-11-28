568525113f8fef822ef14d27c30ad5d52b29d06f
Eric Dahl <eric.c.dahl@gmail.com>
2025-11-28 11:34:18 -0800

    iam node groupo

---
+++ eks_node_group.tf
@@ -0,0 +1,27 @@
+resource "aws_eks_node_group" "default" {
+  cluster_name    = aws_eks_cluster.default.name
+  node_group_name = "${local.name}-node-group"
+  node_role_arn   = aws_iam_role.eks_node_group.arn
+  subnet_ids = [
+    aws_subnet.public_1.id,
+    aws_subnet.public_2.id,
+  ]
+
+  scaling_config {
+    desired_size = 3
+    min_size     = 2
+    max_size     = 4
+  }
+
+  instance_types = ["t3.medium"]
+  capacity_type  = "ON_DEMAND"
+  disk_size      = 20
+
+  ami_type = "AL2_x86_64"
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.eks_container_registry_policy,
+  ]
+}
---
+++ iam_role_eks_node_group.tf
@@ -0,0 +1,32 @@
+data "aws_iam_policy_document" "eks_node_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "eks_node_group" {
+  name               = "${local.name}-eks-node-group"
+  assume_role_policy = data.aws_iam_policy_document.eks_node_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_group.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_group.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_container_registry_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_group.name
+}
